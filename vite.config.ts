af54c91df1b0b990f27cdd3d27bebc2ac45bbf61
titaniper <flutter.kr@gmail.com>
2024-08-13 15:04:43 +0900

    chore: dts 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import path from 'path';
 
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'cjs'], // ES 모듈과 CommonJS 모듈 형식으로 빌드를 수행합니다.
-      fileName: (format) => `index.${format}.js`, // 빌드된 파일 이름 형식 지정
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: [], // 외부 종속성으로 처리할 모듈을 지정할 수 있습니다.
+      external: [], // 외부 종속성을 설정 (필요에 따라 추가)
       output: {
-        globals: {}, // 글로벌 변수를 설정할 수 있습니다 (UMD 형식에서 사용).
+        globals: {}, // 글로벌 변수 설정 (필요에 따라 추가)
       },
     },
   },
+  plugins: [
+    dts({
+      insertTypesEntry: true, // `types` 필드를 생성하고 `package.json`에 추가합니다.
+      outputDir: 'dist', // 타입 정의 파일을 생성할 디렉토리
+    }),
+  ],
 });
